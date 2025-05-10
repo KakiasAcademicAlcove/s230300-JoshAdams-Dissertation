c1efc4572d74036ae14a4a46272cf4198045dc3b
Josh Adams <josh.2.adams@bt.com>
2025-05-10 11:53:16 +0100

    project cleanup

--- inspec/exec.sh
+++ inspec/exec.sh
@@ -3,30 +3,30 @@
 echo 'Running Inspec profiles.'
 
 echo 'Running test profile for FreeRADIUS...'
-inspec exec /share/freeradius --target docker://freeradius --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/freeradius.rb --target docker://freeradius --no-create-lockfile
 
 echo 'Running test profile for Logstash...'
-inspec exec /share/logstash --target docker://logstash --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/logstash.rb --target docker://logstash --no-create-lockfile
 
 echo 'Running test profile for Kafka-1...'
-inspec exec /share/kafka --target docker://kafka-1 --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/kafka.rb --target docker://kafka-1 --no-create-lockfile
 
 echo 'Running test profile for Kafka-2...'
-inspec exec /share/kafka --target docker://kafka-2 --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/kafka.rb --target docker://kafka-2 --no-create-lockfile
 
 echo 'Running test profile for Kafka-3...'
-inspec exec /share/kafka --target docker://kafka-3 --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/kafka.rb --target docker://kafka-3 --no-create-lockfile
 
 echo 'Running test profile for Elasticsearch...'
-inspec exec /share/elasticsearch --target docker://elasticsearch --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/elasticsearch.rb --target docker://elasticsearch --no-create-lockfile
 
 echo 'Running test profile for Kibana...'
-inspec exec /share/kibana --target docker://kibana --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/kibana.rb --target docker://kibana --no-create-lockfile
 
 echo 'Running test profile for Filebeat...'
-inspec exec /share/filebeat --target docker://filebeat --no-create-lockfile &&
+inspec exec $INSPEC_PROFILES_DIR/filebeat.rb --target docker://filebeat --no-create-lockfile
 
 echo 'Running test profile for Metricbeat...'
-inspec exec /share/metricbeat --target docker://metricbeat --no-create-lockfile
+inspec exec $INSPEC_PROFILES_DIR/metricbeat.rb --target docker://metricbeat --no-create-lockfile
 
-echo 'Finished running InSpec profiles. All tests passed!'
+echo 'Finished running InSpec profiles.'
