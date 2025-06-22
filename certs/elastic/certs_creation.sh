37d7fdf16b2976b2768d05f9e77568244cc2f273
Josh Adams <josh.2.adams@bt.com>
2025-06-22 21:19:13 +0100

    updates

--- certs/elastic/certs_creation.sh
+++ certs/elastic/certs_creation.sh
@@ -31,9 +31,12 @@ echo "Setting file permissions"
 chown -R root:root config/certs;
 find . -type d -exec chmod 750 \{\} \;;
 find . -type f -exec chmod 640 \{\} \;;
+
 echo "Waiting for Elasticsearch availability";
 until curl -s --cacert config/certs/ca/ca.crt https://elasticsearch:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
+
 echo "Setting kibana_system password";
 until curl -s -X POST --cacert config/certs/ca/ca.crt -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
 echo "All done!";
+
 touch "$CERT_FLAG"
